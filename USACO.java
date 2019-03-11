95fb4b0c21abce2bd8a7478b6e21db2c03f62e0d
Aryan Sharma <asharma00@stuy.edu>
2019-03-10 23:27:59 -0400

    added bronze comments

--- USACO.java
+++ USACO.java
@@ -5,7 +5,7 @@ public class USACO {
 
 
   public static int bronze(String filename) {
-    //variables for row, col, final elevation, and number of instructs
+    //variables for row, col, final elevation, and number of instructs, and arrays
     int r = 0;
     int c = 0;
     int e = 0;
@@ -17,13 +17,13 @@ public class USACO {
       //get primary info
     File f = new File(filename); //initialize file
     Scanner scanner = new Scanner(f); //read file
-    while (scanner.hasNextLine()) {
-      String[] line = scanner.nextLine().split(" ");
+    while (scanner.hasNextLine()) { //while there is another line
+      String[] line = scanner.nextLine().split(" "); //split it based of spaces
       r = Integer.parseInt(line[0]);
       c = Integer.parseInt(line[1]);
       e = Integer.parseInt(line[2]);
-      n = Integer.parseInt(line[3]);
-      map = new int[r][c];
+      n = Integer.parseInt(line[3]); //parse through to find variables
+      map = new int[r][c]; //make map based off of the r and c
       for (int i = 0; i < r; i++){
         String[] lines = scanner.nextLine().split(" ");
         for (int j = 0; j < c; j++){
@@ -37,10 +37,10 @@ public class USACO {
           for (int j = 0; j < 3; j++) {
             instruct[i][j] = Integer.parseInt(line[j]);
           }
-        }
+        } //make array of instructions based off of the instructions given
       }
-      sink(map, instruct);
-      findElevation(map, e);
+      sink(map, instruct); //the cows stop
+      findElevation(map, e); //find the final elevation after subtrracting
       int total = 0;
       for (int i = 0; i < map.length; i++){
         for (int j = 0; j < map[i].length; j++){
@@ -48,7 +48,7 @@ public class USACO {
         }
       }
       return 72*72*total;
-    }
+    } //get the total voume
 
   catch (FileNotFoundException l){
     System.out.println("file not found");
@@ -62,23 +62,23 @@ public class USACO {
     for (int x = 0; x < instruct.length; x++){
       int r = instruct[x][0];
       int c = instruct[x][1];
-      int e = instruct[x][2];
-      int max = getmax(r, c, map);
-      sinkHelper(r, c, map, e, max);
-        }
+      int e = instruct[x][2]; //for each intrscution, get the r, c, and e
+      int max = getmax(r, c, map); //find the max
+      sinkHelper(r, c, map, e, max); //call helper
+    }
     }
 
     private static int[][] sinkHelper(int r, int c, int[][] map, int e, int max){
-      int low = 0;
-      while (low < e){
+      int num = 0; //varible for
+      while (num < e){
         for (int i = r; i < r + 3; i++ ) {
-          for (int j = c; j < c + 3; j++ ) {
+          for (int j = c; j < c + 3; j++ ) { //3x3 cows
             if (map[i - 1][j - 1] >= max) map[i - 1][j - 1]--;
-          }
+          } //sink by subtracting
         }
-        max = getmax(r, c, map);
+        max = getmax(r, c, map); //what is the new maximum
 
-        low++;
+        num++; //add to the tracker
       }
       return map;
     }
@@ -86,8 +86,8 @@ public class USACO {
   private static void findElevation(int[][] map, int e){
     for (int i = 0; i < map.length; i++){
       for (int j = 0; j < map[i].length; j++){
-        if (map[i][j] > e) map[i][j] = 0;
-        else map[i][j] = e - map[i][j];
+        if (map[i][j] > e) map[i][j] = 0; // if same keep it as 0
+        else map[i][j] = e - map[i][j]; //get the elevation by subtracting based off the given elevation
       }
     }
   }
@@ -96,12 +96,12 @@ public class USACO {
   private static int getmax(int r, int c, int[][] map){
     int max = 0;
     for (int i = r; i < r + 3; i++){
-      for (int j = c ;j < c + 3 ; j++ ) {
+      for (int j = c ;j < c + 3 ; j++ ) { //3x3 cows
         if (max < map[i - 1][j - 1]) {
           max = map[i - 1][j - 1];
         }
       }
-    }
+    } //this is to find the max of the 3x3 stopmong gorunds
     return max;
   }
 
